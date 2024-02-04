ed7222c2c4ee94dc7f24a44f064e7267cd19caeb
jsc <jsc@demigods.org>
2024-02-04 14:50:49 -0800

    Render a triangle to the screen
    This is trivial but shows the main idea. Now we'll have to experiment
    with vertices and stuff.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use url;
 use wasm_bindgen::prelude::*;
 
 // This is recommended for debug builds. Panics will be logged to the console.
@@ -13,6 +14,57 @@ macro_rules! log {
 pub fn main() -> Result<(), JsValue> {
     console_error_panic_hook::set_once();
 
+    let window = web_sys::window()
+        .ok_or(JsValue::from_str("no global window exists"))?;
+    let href = window.location().href()?;
+    let url = url::Url::parse(&href)
+        .or(Err(JsValue::from_str("unable to parse url")))?;
+
+    let document = window
+        .document()
+        .ok_or(JsValue::from_str("should have a document on window"))?;
+    // the intent is to grab it and then we can expand/contract the canvas with this.
+    let container = document.get_element_by_id("illusion-div")
+        .ok_or(JsValue::from_str("unable to locate domino container \"illusion-div\" in document"))?
+        .dyn_into::<web_sys::HtmlElement>()?;
+    let canvas = document.get_element_by_id("phantasm")
+        .ok_or(JsValue::from_str("unable to locate domino canvas \"phantasm\" in document"))?
+        .dyn_into::<web_sys::HtmlCanvasElement>()?;
+
+    let canvas_ctx = canvas
+        .get_context("2d")?
+        .ok_or(JsValue::from_str("unable to retrieve 2d context from domino canvas"))?
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()?;
+
     log!("smoke weed every day");
+
+        let x = 100.0;
+        let y = 100.0;
+        let tile_width = 50.0;
+        let tile_height = 50.0;
+        canvas_ctx.save();
+        {
+            // Set this coordinate to be the origin.
+            canvas_ctx.translate(x, y)
+                .expect("oh god how can this fail?");
+
+            // Draw a line
+            canvas_ctx.begin_path();
+                    canvas_ctx.move_to(0.0, 0.0);
+                    canvas_ctx.line_to(tile_width, 0.0);
+                    canvas_ctx.line_to(tile_width / 2.0, tile_height / 2.0);
+                    canvas_ctx.line_to(0.0, 0.0);
+            canvas_ctx.close_path();
+
+            // This is dumb. Can we really not give it a more direct value?
+            let color = 0x0000ff;
+            let s = format!("#{:0>6x}", color);
+            let color = JsValue::from_str(&s);
+            canvas_ctx.set_fill_style(&color);
+
+            canvas_ctx.fill();
+        }
+        canvas_ctx.restore();
+
     Ok(())
 }
